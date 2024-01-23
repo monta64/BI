e25409e638c695000ad641c0a26b9dd21124d2f1
monta <montassar.benarfia@partner.bmwgroup.com>
2024-01-23 12:11:27 +0100

    [Add project]

---
+++ App.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+#from os import system
+from sys import path
+path.append("C:\\Program Files\\Microsoft.NET\\ADOMD.NET\\160\\")
+from pyadomd import Pyadomd
+
+
+st.set_page_config(page_title="Dashboard", page_icon="🌍", layout="wide")
+
+st.sidebar.markdown(
+    """
+    <style>
+        .sidebar .sidebar-content {
+            background-color: #ffffff; /* White color */
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+CONNECTION_STRING = (
+    "Provider=MSOLAP;Data Source=PRIMA229;Integrated Security=SSPI;Catalog=Tcomms_Cube;"
+)
+
+QUERY_STRING = """
+select 
+{
+[Measures].[Sent],
+[Measures].[Dispatched],
+[Measures].[Deliv],
+[Measures].[Delivery Rate]
+} on columns, 
+NonEmpty
+(([Campaigns].[Prj Mkg].[Prj Mkg],
+[Age Bands].[Age].[Age],
+[Genders].[Gender].[Gender]
+))on Rows
+From TcommsCube
+"""
+
+with Pyadomd(CONNECTION_STRING) as conn:
+    with conn.cursor().execute(QUERY_STRING) as cur:
+        DataSet = cur.fetchall()
+
+# Convert the data to a pandas DataFrame
+df = pd.DataFrame(DataSet)
+df.rename(columns={0:"campaigns", 1:"age_bands", 2:"genders",3:"sent", 4:"dispatched", 5:"deliv",6:"delivRate"}, inplace=True)
+print(df.head())
+# Main content
+
+st.header("ANALYTICS WEB DASHBOARD | INSURANCE KPI & TRENDS ")
+fig2 = px.bar(df, x='campaigns', y='sent', color='age_bands', barmode='group', title='Anaysis results')
+st.plotly_chart(fig2, use_container_width=True)
+st.sidebar.image("data/logo1.png")
+hide_st_style=""" 
+
+<style>
+#MainMenu {visibility:hidden;}
+footer {visibility:hidden;}
+header {visibility:hidden;}
+</style>
+"""
+
--- query.py
+++ query.py
@@ -18,3 +18,44 @@
 # c.execute('select * from insurance order by id asc')
 # data=c.fetchall()
 # return data
+import pandas as pd
+import pyodbc
+import toml
+
+secrets = toml.load("C:/Users/montassar.benarfia/Desktop/school/BI/.streamlit/secrets.toml")
+
+def run_mdx_query(mdx_query, connection_string):
+    connection = pyodbc.connect(connection_string)
+    cursor = connection.cursor()
+    
+    cursor.execute(mdx_query)
+    
+    # Fetch the results into a DataFrame
+    columns = [column[0] for column in cursor.description]
+    results = cursor.fetchall()
+    df = pd.DataFrame.from_records(results, columns=columns)
+    
+    connection.close()
+    
+    return df
+
+# Example usage:
+mdx_query = '''select 
+{
+[Measures].[Sent],
+[Measures].[Dispatched],
+[Measures].[Deliv],
+[Measures].[Delivery Rate]
+} on columns, 
+NonEmpty
+(([Campaigns].[Prj Mkg].[Prj Mkg],
+[Age Bands].[Age].[Age],
+[Genders].[Gender].[Gender]
+))on Rows
+From TcommsCube'''
+connection_string = 'DRIVER={SQL Server};SERVER=PRIMA229;DATABASE=Tcomms_Cube;UID=PRIMATEC\\montassar.benarfia;authentication = "Windows"'
+
+result_df = run_mdx_query(mdx_query, connection_string)
+print('MDX Query Result:')
+print(result_df)
+
